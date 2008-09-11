d4435939fc87cc68c1f3495827e912ef7aab0fb7
Michael Howitz <mh@gocept.com>
2008-09-11 09:52:23 +0000

    Added specialized variant of zope.interface.verify.verifyObject which can handle references and reference collections correctly.

---
+++ src/gocept/reference/verify.py
@@ -0,0 +1,30 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import zope.interface.verify
+import zope.interface.exceptions
+
+import gocept.reference.reference
+
+
+def verifyObject(iface, candidate, tentative=0):
+    "Specialized variant of verifyObject which handles references correctily."
+    class_ = candidate.__class__
+    # zope.interface.verify.verifyClass returns True or raises exception
+    zope.interface.verify.verifyClass(iface, class_, tentative=tentative)
+    for name, desc in iface.namesAndDescriptions(1):
+        if hasattr(candidate, name):
+            continue
+        # if instance does not provide attribute, look on class if it
+        # has a reference or reference collection, as both may raise
+        # an AttributeError when checked on instance.
+        if hasattr(class_, name):
+            attr = getattr(class_, name)
+            if (isinstance(attr, gocept.reference.reference.Reference) or
+                isinstance(
+                    attr, gocept.reference.collection.ReferenceCollection)):
+                continue
+        raise zope.interface.exceptions.BrokenImplementation(iface, name)
+    return True
